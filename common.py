978334a95a6e9da9371cc0fc0f9d4c9e19241bbe
fresk <fresk93@gmail.com>
2020-05-28 19:39:11 +0300

    review fix

--- common.py
+++ common.py
@@ -446,13 +446,13 @@ def github_get_latest_commit_date(repo):
     pushed_at = json.loads(res.content).get("pushed_at")
     return datetime.datetime.fromisoformat(pushed_at.replace("Z", "+00:00"))
 
-def is_student_author_of_commits(repo, student):
+def check_contributors(repo, student):
     """
-    checks if the student is the author of commits, excluding teacher commits
+    checks the list of contributors, there can only be the student and teachers
 
     :param repo: repository name (with organization/owner prefix)
     :param student: dict with a 'github' key
-    :returns: True if student is author of commits, False otherwise
+    :returns: True if there are no extra contributors, False otherwise
     """
     status_headers = {
         "User-Agent": "GitHubGetCommits/1.0",
@@ -460,26 +460,26 @@ def is_student_author_of_commits(repo, student):
         "Accept": "application/vnd.github.v3.raw",
     }
     res = requests_retry_session().get(
-        "https://api.github.com/repos/{}/commits".format(repo),
+        "https://api.github.com/repos/{}/contributors".format(repo),
         headers=status_headers,
         timeout=settings.requests_timeout
     )
     if res.status_code != 200:
         raise Exception("GitHub API reported an error while trying to get info about repository '{}'! Message is '{}' ({}).".format(repo, res.reason, res.status_code))
     res_json = json.loads(res.content)
-    for commit in res_json:
-        author = commit["commit"]["author"]["name"]
-        if author not in settings.teacher_github_logins or author != student["github"]:
+    for contributor in res_json:
+        login = contributor["login"]
+        if login not in settings.teacher_github_logins or login != student["github"]:
             return False
     return True
 
-def is_student_change_tests(lab_id, repo):
+def check_test_unchanged(lab_id, repo):
     """
     check if the student changed tests
 
     :param lab_id: number of lab
     :param repo: repository name (with organization/owner prefix)
-    :returns: True if student changed the tests, False otherwise
+    :returns: True if student did't change the tests, False otherwise
     """
     for file in settings.os_labs[lab_id]['test_files']:
         status_headers = {
@@ -488,7 +488,7 @@ def is_student_change_tests(lab_id, repo):
             "Accept": "application/vnd.github.v3.raw",
         }
         res = requests_retry_session().get(
-            "https://api.github.com/repos/{}/commits?path=tests.sh&path={}".format(repo, file),
+            "https://api.github.com/repos/{}/commits?path={}".format(repo, file),
             headers=status_headers,
             timeout=settings.requests_timeout
         )
@@ -497,7 +497,6 @@ def is_student_change_tests(lab_id, repo):
         res_json = json.loads(res.content)
         for commit in res_json:
             author = commit["commit"]["author"]["name"]
-            print(author)
             if author not in settings.teacher_github_logins:
-                return True
-    return False
+                return False
+    return True
